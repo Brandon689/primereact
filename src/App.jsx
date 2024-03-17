23ce269e656edb931384d4bcaef549aeead2af11
Brandon <brandon139@protonmail.com>
2024-03-17 16:00:41 +1100

    product list component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Monday from './Monday';
 import Tuesday from './Tuesday';
 import Wednesday from './Wednesday';
 import Thursday from './Thursday';
+import DataViewComponent from './DataViewComponent';
 
 export default function BasicDemo() {
     const [selectedContent, setSelectedContent] = useState('content1'); // Initial content
@@ -16,18 +17,29 @@ export default function BasicDemo() {
     };
     return (
         <div className='layout-container layout-wrapper layout-dark layout-static' data-theme="dark">
-            <div className="grid">
-                <div className="col-fixed">
-                    <SidePanel/>
-                    <SideLinks onContentChange={handleContentChange} />
-                </div>
-                <div className="col">
+
+            <aside className="layout-sidebar">
+                <SidePanel onContentChange={handleContentChange} />
+                <SideLinks onContentChange={handleContentChange} />
+            </aside>
+            <div className="layout-content">
+                <div className="layout-content-inner">
+                <DataViewComponent/>
+
                     {selectedContent === 'content1' && <Monday />}
                     {selectedContent === 'content2' && <Tuesday />}
                     {selectedContent === 'content3' && <Wednesday />}
                     {selectedContent === 'content4' && <Thursday />}
                 </div>
             </div>
+            {/* <div className="grid">
+                <div className="col-fixed">
+                    
+                </div>
+                <div className="col">
+                    
+                </div>
+            </div> */}
         </div>
         // <div className="card flex justify-content-center">
         //     <Tree value={nodes} className="w-full md:w-30rem" />
---
+++ src/DataViewComponent.jsx
@@ -0,0 +1,283 @@
+import React, { useState, useEffect } from 'react';
+import { Button } from 'primereact/button';
+import { DataView } from 'primereact/dataview';
+import { Rating } from 'primereact/rating';
+import { Tag } from 'primereact/tag';
+import { classNames } from 'primereact/utils';
+
+export default function DataViewComponent() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        var data = [
+            {
+                id: '1000',
+                code: 'f230fh0g3',
+                name: 'Bamboo Watch',
+                description: 'Product Description',
+                image: 'bamboo-watch.jpg',
+                price: 65,
+                category: 'Accessories',
+                quantity: 24,
+                inventoryStatus: 'INSTOCK',
+                rating: 5
+            },
+            {
+                id: '1001',
+                code: 'nvklal433',
+                name: 'Black Watch',
+                description: 'Product Description',
+                image: 'black-watch.jpg',
+                price: 72,
+                category: 'Accessories',
+                quantity: 61,
+                inventoryStatus: 'INSTOCK',
+                rating: 4
+            },
+            {
+                id: '1002',
+                code: 'zz21cz3c1',
+                name: 'Blue Band',
+                description: 'Product Description',
+                image: 'blue-band.jpg',
+                price: 79,
+                category: 'Fitness',
+                quantity: 2,
+                inventoryStatus: 'LOWSTOCK',
+                rating: 3
+            },
+            {
+                id: '1003',
+                code: '244wgerg2',
+                name: 'Blue T-Shirt',
+                description: 'Product Description',
+                image: 'blue-t-shirt.jpg',
+                price: 29,
+                category: 'Clothing',
+                quantity: 25,
+                inventoryStatus: 'INSTOCK',
+                rating: 5
+            },
+            {
+                id: '1004',
+                code: 'h456wer53',
+                name: 'Bracelet',
+                description: 'Product Description',
+                image: 'bracelet.jpg',
+                price: 15,
+                category: 'Accessories',
+                quantity: 73,
+                inventoryStatus: 'INSTOCK',
+                rating: 4
+            },
+            {
+                id: '1005',
+                code: 'av2231fwg',
+                name: 'Brown Purse',
+                description: 'Product Description',
+                image: 'brown-purse.jpg',
+                price: 120,
+                category: 'Accessories',
+                quantity: 0,
+                inventoryStatus: 'OUTOFSTOCK',
+                rating: 4
+            },
+            {
+                id: '1006',
+                code: 'bib36pfvm',
+                name: 'Chakra Bracelet',
+                description: 'Product Description',
+                image: 'chakra-bracelet.jpg',
+                price: 32,
+                category: 'Accessories',
+                quantity: 5,
+                inventoryStatus: 'LOWSTOCK',
+                rating: 3
+            },
+            {
+                id: '1007',
+                code: 'mbvjkgip5',
+                name: 'Galaxy Earrings',
+                description: 'Product Description',
+                image: 'galaxy-earrings.jpg',
+                price: 34,
+                category: 'Accessories',
+                quantity: 23,
+                inventoryStatus: 'INSTOCK',
+                rating: 5
+            },
+            {
+                id: '1008',
+                code: 'vbb124btr',
+                name: 'Game Controller',
+                description: 'Product Description',
+                image: 'game-controller.jpg',
+                price: 99,
+                category: 'Electronics',
+                quantity: 2,
+                inventoryStatus: 'LOWSTOCK',
+                rating: 4
+            },
+            {
+                id: '1009',
+                code: 'cm230f032',
+                name: 'Gaming Set',
+                description: 'Product Description',
+                image: 'gaming-set.jpg',
+                price: 299,
+                category: 'Electronics',
+                quantity: 63,
+                inventoryStatus: 'INSTOCK',
+                rating: 3
+            },
+            {
+                id: '1010',
+                code: 'plb34234v',
+                name: 'Gold Phone Case',
+                description: 'Product Description',
+                image: 'gold-phone-case.jpg',
+                price: 24,
+                category: 'Accessories',
+                quantity: 0,
+                inventoryStatus: 'OUTOFSTOCK',
+                rating: 4
+            },
+            {
+                id: '1011',
+                code: '4920nnc2d',
+                name: 'Green Earbuds',
+                description: 'Product Description',
+                image: 'green-earbuds.jpg',
+                price: 89,
+                category: 'Electronics',
+                quantity: 23,
+                inventoryStatus: 'INSTOCK',
+                rating: 4
+            },
+            {
+                id: '1012',
+                code: '250vm23cc',
+                name: 'Green T-Shirt',
+                description: 'Product Description',
+                image: 'green-t-shirt.jpg',
+                price: 49,
+                category: 'Clothing',
+                quantity: 74,
+                inventoryStatus: 'INSTOCK',
+                rating: 5
+            },
+            {
+                id: '1013',
+                code: 'fldsmn31b',
+                name: 'Grey T-Shirt',
+                description: 'Product Description',
+                image: 'grey-t-shirt.jpg',
+                price: 48,
+                category: 'Clothing',
+                quantity: 0,
+                inventoryStatus: 'OUTOFSTOCK',
+                rating: 3
+            },
+            {
+                id: '1014',
+                code: 'waas1x2as',
+                name: 'Headphones',
+                description: 'Product Description',
+                image: 'headphones.jpg',
+                price: 175,
+                category: 'Electronics',
+                quantity: 8,
+                inventoryStatus: 'LOWSTOCK',
+                rating: 5
+            },
+            {
+                id: '1015',
+                code: 'vb34btbg5',
+                name: 'Light Green T-Shirt',
+                description: 'Product Description',
+                image: 'light-green-t-shirt.jpg',
+                price: 49,
+                category: 'Clothing',
+                quantity: 34,
+                inventoryStatus: 'INSTOCK',
+                rating: 4
+            },
+            {
+                id: '1016',
+                code: 'k8l6j58jl',
+                name: 'Lime Band',
+                description: 'Product Description',
+                image: 'lime-band.jpg',
+                price: 79,
+                category: 'Fitness',
+                quantity: 12,
+                inventoryStatus: 'INSTOCK',
+                rating: 3
+            },
+        ];
+
+        setProducts(data);
+
+
+    }, []);
+
+    const getSeverity = (product) => {
+        switch (product.inventoryStatus) {
+            case 'INSTOCK':
+                return 'success';
+
+            case 'LOWSTOCK':
+                return 'warning';
+
+            case 'OUTOFSTOCK':
+                return 'danger';
+
+            default:
+                return null;
+        }
+    };
+
+    const itemTemplate = (product, index) => {
+        return (
+            <div className="col-12" key={product.id}>
+                <div className={classNames('flex flex-column xl:flex-row xl:align-items-start p-4 gap-4', { 'border-top-1 surface-border': index !== 0 })}>
+                    <img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round" src={`https://primefaces.org/cdn/primereact/images/product/${product.image}`} alt={product.name} />
+                    <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
+                        <div className="flex flex-column align-items-center sm:align-items-start gap-3">
+                            <div className="text-2xl font-bold text-900">{product.name}</div>
+                            <Rating value={product.rating} readOnly cancel={false}></Rating>
+                            <div className="flex align-items-center gap-3">
+                                <span className="flex align-items-center gap-2">
+                                    <i className="pi pi-tag"></i>
+                                    <span className="font-semibold">{product.category}</span>
+                                </span>
+                                <Tag value={product.inventoryStatus} severity={getSeverity(product)}></Tag>
+                            </div>
+                        </div>
+                        <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
+                            <span className="text-2xl font-semibold">${product.price}</span>
+                            <Button icon="pi pi-shopping-cart" className="p-button-rounded" disabled={product.inventoryStatus === 'OUTOFSTOCK'}></Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
+    const listTemplate = (items) => {
+        if (!items || items.length === 0) return null;
+
+        let list = items.map((product, index) => {
+            return itemTemplate(product, index);
+        });
+
+        return <div className="grid grid-nogutter">{list}</div>;
+    };
+
+    return (
+        <div className="card">
+            <DataView value={products} listTemplate={listTemplate} />
+        </div>
+    )
+}
+         
